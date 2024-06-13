81a5a20bb03240c911996810e17fda4833168daa
callaghan210-coder <skiprop336@gmail.com>
2024-06-13 15:57:31 +0300

    user interface

---
+++ index.js
@@ -0,0 +1,8 @@
+const num1  = math.ceil*(math.random()*10)
+const num2  = math.ceil*(math.random()*10)
+const questionE= document.getElementById("question")
+const formE = document.getElementById("form")
+const inputE = document.getElementById("input")
+const scoreE = document.getElementById("score")
+
+questionE.innerText= `What is $n`
